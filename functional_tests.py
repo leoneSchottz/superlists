1136d0fb7c3d1fc9b01229f79b7ab20d1e2d7f6a
Leone Schottz <schottzleone@gmail.com>
2022-11-30 11:07:33 -0300

    Verifing table content

--- functional_tests.py
+++ functional_tests.py
@@ -13,6 +13,12 @@ class NewVisitorTest(unittest.TestCase):
 
     def tearDown(self):
         self.browser.quit()
+
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element(By.ID,'id_list_table')
+        rows = table.find_elements(By.TAG_NAME,'tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     
     def test_can_start_a_list_and_retrive_it_later(self):
         
@@ -36,18 +42,30 @@ class NewVisitorTest(unittest.TestCase):
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
 
-        table = self.browser.find_element(By.ID,'id_list_table')
-        rows = table.find_elements(By.TAG_NAME,'tr')
-        self.assertTrue(any(row.text == '1: Buy peacock feathers' for row in rows), "New to-do item did not appear in table")
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+        
+        
+        
+        
         #The page still show an text box invites Edith to insert another item
         #Edith types "Use peacock feathers to make a fly"
-        self.fail('Finish the test!')
+        inputbox = self.browser.find_element(By.ID,'id_new_item')
+        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+        self.check_for_row_in_list_table('2: Use peacock feathers to make a fly') 
+        
+
+        
+        
         #The page refreshs again and shows two itens
 
         #Edith wonders if the site remembers her list. So the site generated a url
 
         #She access the url and her list remains there
-
+        self.fail('Finish the test!')
         self.browser.quit()
 
 if __name__ == '__main__':
